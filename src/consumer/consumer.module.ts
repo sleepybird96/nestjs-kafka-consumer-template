f502dd9fd55a99ac4a6ad70baa400a8291e957ec
Jisang Park <iam@park.jisang.dev>
2022-09-15 16:15:17 +0900

    <fix> export consumerService

--- src/consumer/consumer.module.ts
+++ src/consumer/consumer.module.ts
@@ -5,5 +5,6 @@ import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [ConfigModule],
   providers: [ConsumerService],
+  exports: [ConsumerService],
 })
 export class ConsumerModule {}
